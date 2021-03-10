f59056abd705a81bd4db335a1d74107c6d160c43
kavya-ios-ttn <kavya.casshyap@tothenew.com>
2021-03-10 14:19:22 +0530

    zip removed

---
+++ Size Classes:Auto Layout/rotateSmiley/rotateSmiley/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rotateSmiley
+//
+//  Created by TTN on 08/03/21.
+//  Copyright © 2021 TTN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
